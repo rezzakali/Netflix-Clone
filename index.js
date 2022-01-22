499f79a8d5cade3e6b7a74b87e33a669f8492fef
rezzakali <myrezzakalirk@gmail.com>
2022-01-23 01:09:27 +0530

    Netflix Clone

---
+++ index.js
@@ -0,0 +1,65 @@
+const mainQuestion = document.getElementById("mainQuestion");
+const answerDiv = document.getElementById("answer");
+
+mainQuestion.addEventListener("click", () => {
+  if (answerDiv.style.display === "none") {
+    answerDiv.style.display = "block";
+  } else {
+    answerDiv.style.display = "none";
+  }
+});
+
+const mainQuestion1 = document.getElementById("mainQuestion1");
+const answerDiv1 = document.getElementById("answer1");
+
+mainQuestion1.addEventListener("click", () => {
+  if (answerDiv1.style.display === "none") {
+    answerDiv1.style.display = "block";
+  } else {
+    answerDiv1.style.display = "none";
+  }
+});
+
+const mainQuestion2 = document.getElementById("mainQuestion2");
+const answerDiv2 = document.getElementById("answer2");
+
+mainQuestion2.addEventListener("click", () => {
+  if (answerDiv2.style.display === "none") {
+    answerDiv2.style.display = "block";
+  } else {
+    answerDiv2.style.display = "none";
+  }
+});
+
+const mainQuestion3 = document.getElementById("mainQuestion3");
+const answerDiv3 = document.getElementById("answer3");
+
+mainQuestion3.addEventListener("click", () => {
+  if (answerDiv3.style.display === "none") {
+    answerDiv3.style.display = "block";
+  } else {
+    answerDiv3.style.display = "none";
+  }
+});
+
+const mainQuestion4 = document.getElementById("mainQuestion4");
+const answerDiv4 = document.getElementById("answer4");
+
+mainQuestion4.addEventListener("click", () => {
+  if (answerDiv4.style.display === "none") {
+    answerDiv4.style.display = "block";
+  } else {
+    answerDiv4.style.display = "none";
+  }
+});
+
+const mainQuestion5 = document.getElementById("mainQuestion5");
+const answerDiv5 = document.getElementById("answer5");
+
+mainQuestion5.addEventListener("click", () => {
+  if (answerDiv5.style.display === "none") {
+    answerDiv5.style.display = "block";
+  } else {
+    answerDiv5.style.display = "none";
+  }
+});
